a8d4365571fc63f6d48c0e0016502340f26517a7
qinwei <qinwei-jk@360jinrong.net>
2019-05-22 22:00:48 +0800

    add alias

---
+++ src/main/java/com/qinwei/deathnote/beans/Alias/AliasRegistry.java
@@ -0,0 +1,16 @@
+package com.qinwei.deathnote.beans.Alias;
+
+/**
+ * @author qinwei
+ * @date 2019-05-22
+ */
+public interface AliasRegistry {
+
+    void registerAlias(String name, String alias);
+
+    void removeAlias(String alias);
+
+    boolean isAlias(String name);
+
+    String[] getAliases(String name);
+}
---
+++ src/main/java/com/qinwei/deathnote/beans/Alias/SimpleAliasRegistry.java
@@ -0,0 +1,30 @@
+package com.qinwei.deathnote.beans.Alias;
+
+/**
+ * @author qinwei
+ * @date 2019-05-22
+ */
+public class SimpleAliasRegistry implements AliasRegistry {
+
+
+
+    @Override
+    public void registerAlias(String name, String alias) {
+
+    }
+
+    @Override
+    public void removeAlias(String alias) {
+
+    }
+
+    @Override
+    public boolean isAlias(String name) {
+        return false;
+    }
+
+    @Override
+    public String[] getAliases(String name) {
+        return new String[0];
+    }
+}
